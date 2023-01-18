4e3b592331678f5260979227c2095baa95e9ace8
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-18 19:36:14 +0300

    add cards

--- src/Components/Videos/Videos.jsx
+++ src/Components/Videos/Videos.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import Card from "../Card/Card";
 import "./Videos.css";
 
 const Videos = () => {
   return (
     <div>
-      <h1>My Video</h1>
+      <h1>My Videos</h1>
+      <div className="video-cards">
+        <div>
+          <Card heading={"test"} detail={"why its not working"} />
+        </div>
+        <div>
+          <Card heading={"helli"} detail={"why its not working"} />
+        </div>
+        <div>
+          <Card heading={"test"} detail={"why its not working"} />
+        </div>
+      </div>
     </div>
   );
 };
