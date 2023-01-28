a10a237beee02367e0ac46523de5aab3eb16bad9
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-01-28 18:21:51 +0300

    add react player

--- src/Components/Videos/v-card.js
+++ src/Components/Videos/v-card.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./v-card.css";
+import ReactPlayer from "react-player";
 
 const Card = (props) => {
   const { emoji, heading, detail, link, name } = props;
   return (
     <div className="v-card">
-      <img src={emoji} />
+      <ReactPlayer url={link} className="video-player" />
       <span>{heading}</span>
       <span>{detail}</span>
-      <a href={link} target="_blank">
-        <button className="c-button">{name}</button>
-      </a>
     </div>
   );
 };
